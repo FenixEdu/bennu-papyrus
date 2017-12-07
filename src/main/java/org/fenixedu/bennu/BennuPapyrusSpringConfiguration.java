4314dbd21b307a3b3a2ac54da89b4b2de0982557
sergiofbsilva <sergiofbsilva@gmail.com>
2017-12-07 18:07:39 +0000

    first commit

---
+++ src/main/java/org/fenixedu/bennu/BennuPapyrusSpringConfiguration.java
@@ -0,0 +1,7 @@
+package org.fenixedu.bennu;
+
+import org.fenixedu.bennu.spring.BennuSpringModule;
+
+@BennuSpringModule(basePackages = "org.fenixedu.bennu.papyrus", bundles = "BennuPapyrusResources")
+public class BennuPapyrusSpringConfiguration {
+}
