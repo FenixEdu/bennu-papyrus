ee4e6f7a2b78606c2b5c08729946b72047544fd6
Francisco Neves <francisco.neves@tecnico.ulisboa.pt>
2018-07-09 16:02:29 +0100

    Add beans to spring configuration

--- src/main/java/org/fenixedu/bennu/BennuPapyrusSpringConfiguration.java
+++ src/main/java/org/fenixedu/bennu/BennuPapyrusSpringConfiguration.java
@@ -1,7 +1,21 @@
 package org.fenixedu.bennu;
 
 import org.fenixedu.bennu.spring.BennuSpringModule;
+import org.springframework.context.annotation.Bean;
+import pt.ist.papyrus.PapyrusClient;
+import pt.ist.papyrus.PapyrusConfiguration;
+import pt.ist.papyrus.PapyrusSettings;
 
 @BennuSpringModule(basePackages = "org.fenixedu.bennu.papyrus", bundles = "BennuPapyrusResources")
 public class BennuPapyrusSpringConfiguration {
+
+    @Bean
+    public PapyrusClient papyrusClient() {
+        return new PapyrusClient(PapyrusConfiguration.getConfiguration().papyrusUrl(), PapyrusConfiguration.getConfiguration().papyrusToken());
+    }
+
+    @Bean
+    public PapyrusSettings defaultSettings() {
+        return PapyrusSettings.newBuilder().landscape(true).size("A4").pdfA(true).build();
+    }
 }
