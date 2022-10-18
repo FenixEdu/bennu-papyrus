56ac449ee5f7094e97dfa881fee99fad4f2d72c5
João <joao.paulo.reis@tecnico.ulisboa.pt>
2022-10-18 17:12:05 +0100

    Add defaults for required configs

--- src/main/java/org/fenixedu/bennu/BennuPapyrusSpringConfiguration.java
+++ src/main/java/org/fenixedu/bennu/BennuPapyrusSpringConfiguration.java
@@ -1,6 +1,8 @@
 package org.fenixedu.bennu;
 
 import org.fenixedu.bennu.spring.BennuSpringModule;
+import org.fenixedu.commons.configuration.ConfigurationManager;
+import org.fenixedu.commons.configuration.ConfigurationProperty;
 import org.springframework.context.annotation.Bean;
 import pt.ist.papyrus.PapyrusClient;
 import pt.ist.papyrus.PapyrusSettings;
@@ -11,6 +13,15 @@ public class BennuPapyrusSpringConfiguration {
     @Bean
     public PapyrusClient papyrusClient() {
         return new PapyrusClient(); // The template used here has to belong to the SI team
+    }
+
+    @ConfigurationManager(description = "IT services Papyrus team")
+    public interface ConfigurationProperties {
+        @ConfigurationProperty(key = "papyrus.4e4ab43c-9da3-444f-9317-2b6cce7a5cb2.url", defaultValue = "https://master.papyrus-tecnico.al.vps.tecnico.ulisboa.pt")
+        public String getPapyrusUrl();
+
+        @ConfigurationProperty(key = "papyrus.4e4ab43c-9da3-444f-9317-2b6cce7a5cb2.token", defaultValue = "SecretToken")
+        public String getPapyrusToken();
     }
 
     @Bean
